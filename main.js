5ff0fe599ca16ad64f8aefd07633a0725592dcb4
CoderDarshanWaskar12 <111749254+CoderDarshanWaskar12@users.noreply.github.com>
2022-11-28 19:04:35 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
+
 function setup() {
     video = createCapture(VIDEO);
     video.size(550, 500);
@@ -15,12 +19,26 @@ function modelLoaded() {
 
 function draw() {
 background('#00008B');
+fill('#F90093');
+stroke('#F90093');
+document.getElementById("font_size").innerHTML = "Font Size Of The Text Will Be = "+difference+"px";
+    textSize(difference);
+    text('Darshan',50,400);
 }
 
-function gotPoses(results)
-{
-    if(results.length > 0)
-    {
+function gotPoses(results,error){
+    if(error){
+        console.error(error);
+    }
+    if(results.length > 0){
         console.log(results);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+
+        difference = floor(leftWristX - rightWristX);
+
+        console.log("rightWristX = "+results[0].pose.rightWrist.x + " rightWristY = "+results[0].pose.rightWrist.y);
+        console.log("leftWristX = "+results[0].pose.leftWrist.x + " leftWristY = "+results[0].pose.leftWrist.y);
     }
 }
